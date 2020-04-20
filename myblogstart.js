96563c35b1c096d2f31d8c70d1e882473f144535
bensharp <xiabinbx1@163.com>
2020-04-20 15:51:43 +0800

    20200420

--- myblogstart.js
+++ myblogstart.js
@@ -2,12 +2,15 @@ const express = require('express');
 const path=require('path');
 const config=require(path.join(__dirname, "./config.js"));
 const makehtml=require(path.join(__dirname, "./htmls/index.js"));
-
+const cookieParser= require('cookie-parser');
 
 const app = express();
-
+app.use(cookieParser());
 app.use(config.mainpage, function (req, res) {
   console.log('456');
+  //res.clearCookie()
+ res.cookie("name",'zhangsan',{maxAge: 900000, httpOnly: true});
+  console.log();
  res.send(makehtml.makehtmlplus(''));
    });
 
